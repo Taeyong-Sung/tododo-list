6f69ffb6fc1467e08ccd1b1ecf3a964d20577161
Taeyong Sung <canny0201@gmail.com>
2024-09-24 03:54:48 -0400

    create

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
+  path('todos/', views.todo_index, name='todo-index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -4,9 +4,24 @@ from django.shortcuts import render
 # Add the following import
 from django.http import HttpResponse
 
-# Define the home view
+# Define the views
 def home(request):
   return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
 
 def about(request):
   return render(request, 'about.html')
+
+def todo_index(request):
+  return render(request, 'todos/index.html', { 'todos': todos })
+
+def home(request):
+  return render(request, 'home.html')
+
+class Todo:
+  def __init__(self, name, description):
+    self.name = name
+    self.description = description
+
+todos = [
+  Todo('Kitchen', 'Clean Stove',),
+]
