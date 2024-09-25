28c3685607c8942a1a56836adc0b10b7a49339f8
Taeyong Sung <canny0201@gmail.com>
2024-09-25 13:28:28 -0400

    deploying app

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn tododolist.wsgi
--- tododolist/settings.py
+++ tododolist/settings.py
@@ -2,6 +2,7 @@ import environ
 import os
 env = environ.Env()
 from pathlib import Path
+import dj_database_url
 
 """
 Django settings for tododolist project.
@@ -28,9 +29,10 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if not 'ON_HEROKU' in os.environ:
+    DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -43,10 +45,12 @@ INSTALLED_APPS = [
 	'django.contrib.sessions',
 	'django.contrib.messages',
 	'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -79,17 +83,27 @@ WSGI_APPLICATION = 'tododolist.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env('NEON_DATABASE_NAME'),
-        'USER': env('NEON_USER'),
-        'PASSWORD': env('NEON_PASSWORD'),
-        'HOST': env('NEON_HOST'),
-        'PORT': '5432',
-        'OPTIONS': {'sslmode': 'require'}
+if 'ON_HEROKU' in os.environ:
+    DATABASES = {
+        "default": dj_database_url.config(
+            env='DATABASE_URL',
+            conn_max_age=600,
+            conn_health_checks=True,
+            ssl_require=True,
+        ),
     }
-}
+else:
+    DATABASES = {
+        'default': {
+		        'ENGINE': 'django.db.backends.postgresql',
+		        'NAME': env('NEON_DATABASE_NAME'),
+		        'USER': env('NEON_USER'),
+		        'PASSWORD': env('NEON_PASSWORD'),
+		        'HOST': env('NEON_HOST'),
+		        'PORT': '5432',
+		        'OPTIONS': {'sslmode': 'require'}
+		    }
+    }
 
 
 # Password validation
@@ -127,7 +141,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 LOGIN_REDIRECT_URL = 'todo-index'
 LOGOUT_REDIRECT_URL = 'home'
 LOGIN_URL = 'home'
